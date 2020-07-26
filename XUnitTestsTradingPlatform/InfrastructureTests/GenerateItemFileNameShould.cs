3d5fe4740573daa9dd3c412a081939acbb0adcfe
sober607 <sober607@gmail.com>
2020-07-26 21:45:52 +0300

    Commit six xUnitTests items fixed

---
+++ XUnitTestsTradingPlatform/InfrastructureTests/GenerateItemFileNameShould.cs
@@ -0,0 +1,31 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Microsoft.AspNetCore.Hosting;
+using TradingPlatform.Infrastructure.Services.Implementations;
+
+namespace XUnitTestsTradingPlatform.InfrastructureTests
+{
+    public class GenerateItemFileNameShould
+    {
+        [Fact]
+        public void ReturnFileName()
+        {
+            //Arrange
+            var mock = new Mock<IWebHostEnvironment>();
+            var testFileName = "Somefile.exe";
+            var expectedRegexPattern =
+@"[0-9a-fA-F]{8}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{4}\-[0-9a-fA-F]{12}";
+            FileService fileservice = new FileService(mock.Object);
+
+            //act
+            var generatedFileName =
+fileservice.GenerateItemFileName(testFileName);
+
+            //assert
+            Assert.Matches(expectedRegexPattern, generatedFileName);
+        }
+    }
+}
---
+++ XUnitTestsTradingPlatform/InfrastructureTests/UploadImageAsyncShould.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using TradingPlatform.Infrastructure.Services.Implementations;
+using Xunit;
+
+namespace XUnitTestsTradingPlatform.InfrastructureTests
+{
+    public class UploadImageAsyncShould
+    {
+        [Fact]
+        public void ReturnNotNull()
+        {
+            //arrange
+            var mockIwebHost = new Mock<IWebHostEnvironment>();
+            var fileMock = new Mock<IFormFile>();
+            var content = "Hello World from a Fake File";
+            var fileName = "test.pdf";
+            var ms = new MemoryStream();
+            var writer = new StreamWriter(ms);
+            writer.Write(content);
+            writer.Flush();
+            ms.Position = 0;
+            fileMock.Setup(t => t.OpenReadStream()).Returns(ms);
+            fileMock.Setup(t => t.FileName).Returns(fileName);
+            fileMock.Setup(t => t.Length).Returns(ms.Length);
+
+            FileService sut = new FileService(mockIwebHost.Object);
+
+            //act
+            var result = sut.UploadImageAsync(fileMock.Object);
+
+            //result
+            Assert.NotNull(result);
+        }
+
+
+    }
+}
